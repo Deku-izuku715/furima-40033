b50538bb3bcad3dbec32e29d477404dd891cc15b
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-03-06 14:37:17 +0900

    コメントの削除、Fakerの導入と実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {'namae'}
-    email                 {'sample@mail'}
+    email                 {Faker::Internet.email}
     password              {'000aaa'}
     password_confirmation {password}
     last_name             {'佐藤'}
