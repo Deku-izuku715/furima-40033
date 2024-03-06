758a52c09c5bd4167bd5316594532cea95a9e2b1
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-03-06 19:25:20 +0900

    単体テストコードの追記、修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'namae'}
-    email                 {Faker::Internet.email}
-    password              {'000aaa'}
-    password_confirmation {password}
-    last_name             {'佐藤'}
-    first_name            {'二郎'}
-    last_name_kana        {'サトウ'}
-    first_name_kana       {'ジロウ'}
-    birth_date            {'2002-02-02'}
+    nickname              { Faker::Name.initials(number: 2)}
+    email                 { Faker::Internet.email}
+    password              { 'a1' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '佐藤' }
+    first_name            { '二郎' }
+    last_name_kana        { 'サトウ' }
+    first_name_kana       { 'ジロウ' }
+    birth_date            { Faker::Date.birthday }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
          @user.email = 'testmail'
          @user.valid?
          expect(@user.errors.full_messages).to include('Email is invalid')
-       end
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
@@ -50,6 +50,69 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '1234567'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordは半角英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordは半角数字のみでは登録できない' do
+        @user.password = '010101'
+        @user.password_confirmation = '010101'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordに全角文字は使用できない' do
+        @user.password = 'aaａ11１'
+        @user.password_confirmation = 'aaａ11１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'last_nameは全角（漢字、ひらがな、カタカナ）で入力しないと登録できない' do
+        @user.last_name = 'ｻﾄｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name 全角文字を使用してください")
+      end
+      it 'fisrt_nameは全角（漢字、ひらがな、カタカナ）で入力しないと登録できない' do
+        @user.first_name = 'ｼﾞﾛｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name 全角文字を使用してください")
+      end
+      it 'last_name_kanaが空では登録できない' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it 'last_name_kanaは全角カタカナでないと登録できない' do
+        @user.last_name_kana = 'ｻﾄｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana 全角カタカナのみで入力してください")
+      end
+      it 'first_name_kanaは全角カタカナでないと登録できない' do
+        @user.first_name_kana = 'ｼﾞﾛｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana 全角カタカナのみで入力してください")
+      end
+      it 'birth_dateが空だと登録できない' do
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
     end
   end
