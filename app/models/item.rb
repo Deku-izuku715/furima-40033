2120b036c698ae52b210b525524679984bb9a80f
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-03-17 15:37:46 +0900

    activehashの記入場所変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,24 @@
 class Item < ApplicationRecord
-  validates :item_name, presence: true
-  validates :item_describe, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :send_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :arrive_day_id, presence: true
+  with_options presence: true
+    validates :item_name
+    validates :item_describe
+    validates :category_id
+    validates :condition_id
+    validates :send_cost_id
+    validates :prefecture_id
+    validates :arrive_day_id
+    validates :image
+  end
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
-  validates :image, presence: true
   belongs_to :user
   #has_one :buy_record
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :send_cost, :prefecture, :arrive_day
+
+  validates :title, :text, presence: true
+
+  validates :category_id, :condition_id, :send_cost_id, :prefecture_id, :arrive_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
