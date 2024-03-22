461fc9bb011828a2fea7fee1bf45da1c425adc2b
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-03-22 21:46:26 +0900

    updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :price, :item_describe, :category_id, :condition_id, :send_cost_id, :prefecture_id, :arrive_day_id, :image).merge(user_id: current_user.id)
