901f7b9ae6fb8ffc6c286243202f408712f220c6
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-03-30 01:18:53 +0900

    販売価格に表示される円を削除

--- app/javascript/item_profit.js
+++ app/javascript/item_profit.js
@@ -1,9 +1,10 @@
-window.addEventListener('turbo:load', () => {
+window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
-    const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
+    const ProfitDom = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    ProfitDom.innerHTML = Math.floor(inputValue) - Math.floor(inputValue * 0.1 );
   })
 });
