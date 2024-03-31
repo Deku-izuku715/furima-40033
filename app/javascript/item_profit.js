416de75c55641e0ba634b5c44597933f9038c42d
Deku-izuku715 <151930109+Deku-izuku715@users.noreply.github.com>
2024-04-01 00:52:53 +0900

    エラーハンドリングによって出品ページに戻った後に価格を際入力しても販売手数料や利益が自動入力される

--- app/javascript/item_profit.js
+++ app/javascript/item_profit.js
@@ -1,10 +1,13 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const ProfitDom = document.getElementById("profit");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    ProfitDom.innerHTML = Math.floor(inputValue) - Math.floor(inputValue * 0.1 );
+const price = () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const ProfitDom = document.getElementById("profit");
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+      ProfitDom.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1 ));
   })
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render",price);
